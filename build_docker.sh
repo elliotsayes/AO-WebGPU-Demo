e3f0e566e93a8341454932e40992294fb2749249
Elliot Sayes <elliot@sayes.cloud>
2024-12-06 23:53:41 +0800

    More precise patching

--- build_docker.sh
+++ build_docker.sh
@@ -5,11 +5,16 @@ ROOT_DIR=$(pwd)
 # cd emsdk/docker
 # make build version=sdk-main-64bit
 
-# cd $ROOT_DIR
+cd $ROOT_DIR
 
-# copy over the patched emscripten WebGPU library file
-mkdir -p ao/dev-cli/container/src/emscripten
-cp -r emscripten/src ao/dev-cli/container/src/emscripten
+# clean up the last emscripten source directory
+rm -rf ao/dev-cli/container/src/emscripten/src
+mkdir -p ao/dev-cli/container/src/emscripten/src
+
+# use git to find changed files since tag 3.1.73 and copy them over
+cd emscripten
+git diff 3.1.73 --name-only | xargs -I {} cp {} ../ao/dev-cli/container/src/emscripten/src
+cd $ROOT_DIR
 
 cd ao/dev-cli/container
 ./build.sh
